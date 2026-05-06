68cd5a5a15d74d9de12d58cb683f21ab4882c08e
Arslan Anwar <arslan_pumit@yahoo.com>
2026-05-07 02:10:51 +0500

    Fix Firebase Android setup, added google-services.json file for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,7 +40,15 @@ android {
         }
     }
 }
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
 
+    // Add Firebase dependencies (without version numbers when using BoM)
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-analytics")
+}
 flutter {
     source = "../.."
 }
